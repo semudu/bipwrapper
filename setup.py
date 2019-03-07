b17f202bfeafbf352f852465774bc1442531f452
Serhat Durmaz <serhat.md@gmail.com>
2019-03-07 22:30:52 +0300

    bip-api to bipapi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bip-api-semudu",
+    name="bipapi-semudu",
     version="0.0.1",
     author="Serhat Durmaz",
     author_email="serhat.md@gmail.com",
     description="BIP Messanger API Wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/semudu/bip-api",
+    url="https://github.com/semudu/bipapi",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
