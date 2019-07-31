0d48623e3c8a3d8aad8148a09b9bd2aa4cb925c4
Serhat Durmaz <serhat.md@gmail.com>
2019-07-31 14:49:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from bipwrapper.api import BIP, MessageType
 
-bip = BIP("http://tims.turkcell.com.tr/tes/rest/spi/sendmsgserv", "bu2705615192872148", "1020velo")
+bip = BIP("http://tims.turkcell.com.tr/tes/rest/spi/sendmsgserv", "**", "***")
 
 bip.send_text_message("5332108311", "test2")
