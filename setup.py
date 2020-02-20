5d8e951d9e08abed319aabf71891866393b04c07
Serhat Durmaz <serhat.md@gmail.com>
2020-02-20 22:03:39 +0300

    pypi upload

--- setup.py
+++ setup.py
@@ -5,20 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bipwrapper",
-    version="0.0.5",
+    version="1.0.1",
     description="BIP Messenger API Wrapper",
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Serhat Durmaz",
     author_email="serhat.md@gmail.com",
     url="https://github.com/semudu/bipwrapper",
-    packages=setuptools.find_packages(),
-    license="MIT",
+    download_url="https://github.com/semudu/bipwrapper/archive/v1.0.1.tar.gz",
+    keywords=["BIP", "WRAPPER", "API"],
+    install_requires=["requests"],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-    ],
-    include_package_data=True,
-    zip_safe=False
+    ]
 )
